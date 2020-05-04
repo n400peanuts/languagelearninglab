776b923cf25a1c8064a34e8bf7d65f61649d9c10
Eva <n400peanuts@gmail.com>
2020-05-04 17:37:14 +0100

    lab pic added

---
+++ tools/lizCenter.R
@@ -0,0 +1,10 @@
+lizCenter= function(x, listfname) 
+{
+  for (i in 1:length(listfname)) 
+  {
+    fname = as.character(listfname[i])
+    x[paste(fname,".ct", sep="")] = myCenter(x[fname])
+  }
+  
+  return(x)
+}
